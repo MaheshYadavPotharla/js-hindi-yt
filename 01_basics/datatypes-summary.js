ea48fee576c95cab3d49cb2512af8d1cbcb2998e
MaheshYadavPotharla <maheshyadavpotharla10@gmail.com>
2025-01-03 13:35:15 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,18 @@
+//Primitive
+
+//7 types  --- *string   *number   *boolean    *bigint    *symbol      *null    *undefined
+
+// Non-primitive   --    *arrays       *objects     *functions
+
+
+const heroes = ["shaktiman", "Batman", "superman"];
+
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
