7a54f4a502960bc1d5d464745880421e9aca2f26
MaheshYadavPotharla <maheshyadavpotharla10@gmail.com>
2025-01-09 07:11:42 +0000

    slice

---
+++ 02_basics/slice&splice.js
@@ -0,0 +1,23 @@
+// arr = [4, 7, 81, 10]
+
+// console.log(arr.slice(0, 2));
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango", "tamarind", "pickle"];
+
+//The splice() method adds or removes array elements.
+//at position 2 remove 0 nd add two fruits "lemon" and "kiwi"
+
+
+console.log(fruits.splice(-2, 1, "lemon", "kiwi"));
+
+console.log(fruits);
+
+
+//The splice() method adds and/or removes array elements.
+
+// The splice() method overwrites the original array.
+
+// console.log(fruits.slice(2, 4));
+// console.log(fruits);
+
