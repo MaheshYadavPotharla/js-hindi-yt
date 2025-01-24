41545efd95b40228ce5aabdd8c1920fd7370cb39
MaheshYadav <maheshyadavpotharla10@gmail.com>
2025-01-24 05:33:05 +0000

    switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,33 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 31
+
+switch (month) {
+
+    case 1:
+        console.log("january");
+        
+        break;
+    case 2:
+        console.log("february");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+
+        
+    default:
+        console.log("please enter a valid month");
+        
+        break;
+}
