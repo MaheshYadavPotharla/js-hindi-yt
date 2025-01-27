e6d4746e922072acdc4e0358a5ba1db23f303209
MaheshYadav <maheshyadavpotharla10@gmail.com>
2025-01-27 12:42:05 +0000

    higher_order_functions_4

---
+++ 05_iterations/04_higher_order functions.js
@@ -0,0 +1,19 @@
+const myObject = {
+js: 'javascript',
+cpp: 'c ++',
+rb: 'ruby',
+swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+   console.log(myObject[key]);
+   
+}
+
+
+const programming = ["js", "py", "rb", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+    
+}
