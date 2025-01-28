5dc82b2e20f82cbae356352134d35d467025d0b4
MaheshYadav <maheshyadavpotharla10@gmail.com>
2025-01-28 06:50:34 +0000

    filter

---
+++ 05_iterations/06_Map_Filter_Reduce.js
@@ -0,0 +1,20 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+
+
+// const values = coding.forEach(  (item) => {
+//     console.log(item);
+    
+//     return item
+// })
+
+// console.log(values);
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+
+const NewNums = myNums.filter( (num) => num > 4)
+
+console.log(NewNums);
